412eabd625820b28aa9b02cd9023d3864d4331a5
Robert Baker <reiichi001@gmail.com>
2024-01-12 14:03:34 -0800

    fix typo
    Whoops, left dev code in place. Swapped to prod.

--- index.js
+++ index.js
@@ -26,21 +26,20 @@ async function listPullRequests(token, repoOwner, repo) {
 
 async function run() {
 	try {
-		/*
 		const token = core.getInput('token');
 		const webhook = new WebhookClient({
 			url: core.getInput('discord_webhook'),
 		});
 		const pacSheetsLink = core.getInput('pacsheetslink');
-		*/
 
+		/*
 		require('dotenv').config();
 		const token = process.env.GITHUB_ACCESS_TOKEN;
 		const webhook = new WebhookClient({
 			url: process.env.DISCORD_WEBHOOK_URL,
 		});
 		const pacSheetsLink = process.env.PAC_SHEETS_LINK;
-
+		*/
 
 		const repoOwner = github.context.repo.owner;
 		const repo = github.context.repo.repo;
